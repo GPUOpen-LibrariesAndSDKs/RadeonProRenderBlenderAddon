1e6dcfb5d6a8b6a107846c0f601c21b635334ef8
tnarine-amd <tracy.narine@amd.com>
2017-08-08 14:55:32 -0400

    Adding two missing files.

---
+++ src/rprblender/render/device.py
@@ -0,0 +1,269 @@
+import gc
+
+import numpy as np
+
+import pyrpr
+from pyrpr import ffi
+import pyrprx
+
+import rprblender.render
+from rprblender import config, logging, images
+from rprblender.helpers import CallLogger
+
+logged = CallLogger(tag='render.device').logged
+
+
+class AOV:
+
+    def __init__(self, aov_name, context, render_resolution):
+        self.context = context
+        self.aov = convert_name_to_rpr_aov(aov_name)
+        desc = ffi.new("rpr_framebuffer_desc*")
+        self.width, self.height = render_resolution
+        desc.fb_width, desc.fb_height = self.width, self.height
+
+        fmt = (4, pyrpr.COMPONENT_TYPE_FLOAT32)
+        self.render_buffer = pyrpr.FrameBuffer()
+        pyrpr.ContextCreateFrameBuffer(context, fmt, desc, self.render_buffer)
+        self.core_context = context
+
+        self.attached = False
+
+    def attach(self):
+        assert not self.attached
+        pyrpr.ContextSetAOV(self.core_context, self.aov, self.render_buffer)
+        self.attached = True
+
+    def detach(self):
+        if self.attached:
+            pyrpr.ContextSetAOV(self.context, self.aov, ffi.NULL)
+            self.attached = False
+
+    def clear(self):
+        pyrpr.FrameBufferClear(self.render_buffer)
+
+    def get_core_frame_buffer_image(self):
+        return get_core_frame_buffer_image(self.width, self.height, self.render_buffer)
+
+    def __del__(self):
+        self.detach()
+
+
+class PostEffect:
+
+    def __init__(self, core_post_effect):
+        self.core_post_effect = core_post_effect
+
+    def set_param_float(self, name, value):
+        pyrpr.PostEffectSetParameter1f(self.core_post_effect, name, value)
+
+    def set_param_int(self, name, value):
+        pyrpr.PostEffectSetParameter1u(self.core_post_effect, name, value)
+
+
+class PostEffectUpdate:
+
+    def __init__(self, render_device):
+        self.posteffects_needed = []
+        self.render_device = render_device
+
+    def enable(self, post_effect_name) -> PostEffect:
+        self.posteffects_needed.append(post_effect_name)
+        return PostEffect(self.render_device.attach_posteffect(post_effect_name))
+
+
+class PostEffectChain:
+
+    def __init__(self, render_device):
+        self.render_device = render_device
+
+    def start_update(self):
+        # remove all posteffects
+        # TODO: possible optimization is to leave this for later
+        # and don't delete used effects. BUT post-effects attachments order matters.
+        # this will make this code a bit more complex. Right now I don't see need for extra complexity.
+        for post_effect in list(self.render_device.post_effects):
+            self.render_device.detach_posteffect(post_effect)
+
+        return PostEffectUpdate(self.render_device)
+
+
+class RenderTargets:
+
+    def __init__(self, render_device, render_resolution):
+        self.render_device = render_device
+
+        self.frame_buffer_tonemapped = None
+
+        self.aovs = {}
+
+        self.attached = False
+
+        self.render_resolution = render_resolution
+
+        # create fb for applying posteffects
+        desc = ffi.new("rpr_framebuffer_desc*")
+        desc.fb_width, desc.fb_height = render_resolution
+        fmt = (4, pyrpr.COMPONENT_TYPE_FLOAT32)
+        self.frame_buffer_tonemapped = pyrpr.FrameBuffer()
+        pyrpr.ContextCreateFrameBuffer(self.render_device.core_context, fmt, desc, self.frame_buffer_tonemapped)
+
+
+    @logged
+    def __del__(self):
+        self.aovs.clear()
+        del self.frame_buffer_tonemapped
+
+    def attach(self):
+        assert not self.attached
+        for aov in self.aovs.values():
+            aov.attach()
+        self.attached = True
+
+    def detach(self):
+        if self.attached:
+            for aov in self.aovs.values():
+                aov.detach()
+            self.attached = False
+
+    def clear(self):
+        for aov in self.aovs.values():
+            aov.clear()
+
+    def get_image(self, aov_name='default'):
+        name = aov_name
+        if name not in self.aovs:
+            return None
+        with rprblender.render.core_operations(raise_error=True):
+            fb_image = self.aovs[name].get_core_frame_buffer_image()
+
+        return fb_image
+
+    def get_frame_buffer(self, aov_name='default'):
+        try:
+            return self.aovs[aov_name].render_buffer
+        except KeyError:pass
+
+    def get_resolved_image(self, fb):
+        pyrpr.FrameBufferClear(self.frame_buffer_tonemapped)
+        pyrpr.ContextResolveFrameBuffer(self.render_device.core_context, fb, self.frame_buffer_tonemapped)
+        return get_image(*self.render_resolution, self.frame_buffer_tonemapped)
+
+    def enable_aov(self, aov_name):
+        if aov_name in self.aovs:
+            return
+
+        aov = AOV(aov_name, self.render_device.core_context, self.render_resolution)
+        self.aovs[aov_name] = aov
+
+        if self.attached:
+            aov.attach()
+
+        logging.info('added aov: "%s", ok' % aov_name)
+
+    def disable_aov(self, aov_name):
+        if aov_name in self.aovs:
+            logging.info('remove: ', aov_name)
+            self.aovs.pop(aov_name).detach()
+
+    def is_aov_enabled(self, aov_name):
+        return aov_name in self.aovs
+
+
+class RenderDevice:
+
+    @logged
+    def __init__(self, is_production, context_flags):
+        self.core_context = rprblender.render.create_context(rprblender.render.ensure_core_cache_folder(), context_flags)
+        pyrpr.ContextSetParameter1u(self.core_context, b'xflip', 0)
+        pyrpr.ContextSetParameter1u(self.core_context, b'yflip', 1)
+        pyrpr.ContextSetParameter1u(self.core_context, b'preview', 0 if is_production else 1)
+        self.core_material_system = pyrpr.MaterialSystem()
+        pyrpr.ContextCreateMaterialSystem(self.core_context, 0, self.core_material_system)
+
+        self.core_uber_rprx_context = pyrprx.Object('rprx_context')
+        pyrprx.CreateContext(self.core_material_system, 0, self.core_uber_rprx_context)
+
+        self.post_effects = {}
+
+        self.render_target = None  # type:RenderTargets
+
+    @logged
+    def __del__(self):
+        images.core_image_cache.purge_for_context(self.core_context)
+
+        del self.core_material_system
+        del self.post_effects
+
+        pyrprx.DeleteContext(self.core_uber_rprx_context)
+        del self.core_uber_rprx_context
+
+        if config.debug:
+            referrers = gc.get_referrers(self.core_context)
+            assert 1 == len(referrers), (referrers, self.core_context)
+
+    def attach_render_target(self, render_target):
+        if self.render_target:
+            self.render_target.detach()
+        self.render_target = render_target
+        self.render_target.attach()
+
+    def detach_render_target(self, render_target):
+        self.render_target.detach()
+        self.render_target = None
+
+    def attach_posteffect(self, name):
+        if name not in self.post_effects:
+            post_effect = pyrpr.PostEffect()
+            self.post_effects[name] = post_effect
+            pyrpr.ContextCreatePostEffect(self.core_context, name, post_effect)
+            pyrpr.ContextAttachPostEffect(self.core_context, post_effect)
+        return self.post_effects[name]
+
+    def detach_posteffect(self, post_effect):
+        pyrpr.ContextDetachPostEffect(self.core_context, self.post_effects[post_effect])
+        self.post_effects[post_effect].delete()
+        del self.post_effects[post_effect]
+
+
+@logged
+def get_core_frame_buffer_image(width, height, render_buffer):
+    fb_data_size_ptr = ffi.new('size_t*', 0)
+    pyrpr.FrameBufferGetInfo(render_buffer, pyrpr.FRAMEBUFFER_DATA, 0, ffi.NULL, fb_data_size_ptr);
+
+    fb_data_size = fb_data_size_ptr[0]
+
+    arr = np.empty((height, width, 4), dtype=np.float32)
+    assert arr.nbytes == fb_data_size, (arr.nbytes, fb_data_size)
+
+    pyrpr.FrameBufferGetInfo(render_buffer, pyrpr.FRAMEBUFFER_DATA, fb_data_size,
+                             ffi.cast('float*', arr.ctypes.data), ffi.NULL);
+    return arr
+
+
+def get_image(width, height, render_buffer):
+    return get_core_frame_buffer_image(width, height, render_buffer)
+
+
+def convert_name_to_rpr_aov(name):
+    if name == 'default':
+        return pyrpr.AOV_COLOR
+    elif name == 'opacity':
+        return pyrpr.AOV_OPACITY
+    elif name == 'world_coordinate':
+        return pyrpr.AOV_WORLD_COORDINATE
+    elif name == 'uv':
+        return pyrpr.AOV_UV
+    elif name == 'material_idx':
+        return pyrpr.AOV_MATERIAL_IDX
+    elif name == 'geometric_normal':
+        return pyrpr.AOV_GEOMETRIC_NORMAL
+    elif name == 'shading_normal':
+        return pyrpr.AOV_SHADING_NORMAL
+    elif name == 'depth':
+        return pyrpr.AOV_DEPTH
+    elif name == 'object_id':
+        return pyrpr.AOV_OBJECT_ID
+    assert False
+
+
---
+++ src/rprblender/version_checking.py
@@ -0,0 +1,236 @@
+#!python3
+import bpy
+import os
+import json
+import tempfile
+import threading
+import time
+import traceback
+import urllib.request, urllib.error
+
+from . import rpraddon
+from . import logging
+from .nodes import RPRPanel
+from . import config
+from . import versions
+from . import helpers
+
+
+class DownloadAddonJsonThread(threading.Thread):
+    def __init__(self):
+        self.json_obj = None
+        super().__init__()
+
+    def run(self):
+        self.json_obj = None
+        logging.info('DownloadAddonJsonThread started...')
+        try:
+            url = config.url_json_version
+            logging.info('Download from: ', url)
+
+            with urllib.request.urlopen(url, timeout=60) as url_handle:
+                data = url_handle.read()
+                str = data.decode("utf-8")
+                self.json_obj = json.loads(str)
+                logging.info('thread json_obj: ', self.json_obj)
+            # time.sleep(5)
+        except:
+            logging.error("Can't download json file. ")
+            #print("Unexpected exception: " + traceback.format_exc())
+        finally:
+            logging.info('DownloadAddonJsonThread finished.')
+
+
+class DownloadAddonMsiThread(threading.Thread):
+    def __init__(self, url):
+        self.url = url
+        super().__init__()
+        self.downloaded = 0
+        self.total_size = 0
+        self.terminate = False
+        self.msi_file_name = ''
+
+    def run(self):
+        logging.info('DownloadAddonMsiThread started...')
+        try:
+            block_size = 32768
+            tmp_file = tempfile.NamedTemporaryFile(prefix='rpr_', suffix='.msi', delete=False)
+            logging.info('Save to temporary file: ', tmp_file.name)
+            logging.info('Download from: ', self.url)
+
+            with urllib.request.urlopen(self.url, timeout=60) as response:
+                self.total_size = int(response.getheader("Content-Length"))
+                logging.info('file_size: ', self.total_size)
+                self.downloaded = 0
+                while True:
+                    if self.terminate:
+                        logging.info('   DownloadAddonMsiThread was terminated')
+                        break
+                    buffer = response.read(block_size)
+                    if not buffer:
+                        break
+                    tmp_file.write(buffer)
+                    self.downloaded += len(buffer)
+
+            tmp_file.close()
+
+            if self.terminate:
+                os.unlink(tmp_file.name)
+            else:
+                self.msi_file_name = tmp_file.name
+        except:
+            logging.error("Can't download Msi file. ")
+            #print("Unexpected exception: " + traceback.format_exc())
+        finally:
+            logging.info('DownloadAddonMsiThread finished.')
+
+
+
+@rpraddon.register_class
+class RPRRender_PT_Update(RPRPanel, bpy.types.Panel):
+    bl_label = "RPR Update Addon"
+
+    json_obj = None
+    json_thread = None
+
+    def check_updates(self):
+        # need check delay
+        if not self.json_thread:
+            self.json_thread = DownloadAddonJsonThread()
+            self.json_thread.start()
+        elif not self.json_thread.isAlive():
+            self.json_obj = self.json_thread.json_obj
+
+    @classmethod
+    def poll(cls, context):
+        cls.check_updates(cls)
+
+        # check update version
+        if cls.json_obj:
+            current_version = versions.get_addon_version()
+            update_version = cls.json_obj['version']
+            v = tuple(int(p) for p in update_version.split('.'))
+            if not versions.is_older_than_version(current_version, v):
+                return False
+
+        # hide all menus
+        if cls.json_obj and cls.is_notify(cls):
+            RPRPanel.hide_rpr_ui = cls.json_obj['mustUpdate'] == 'true'
+        else:
+            RPRPanel.hide_rpr_ui = False
+
+        rd = context.scene.render
+        return cls.json_obj and rd.engine in cls.COMPAT_ENGINES
+
+    def is_notify(self):
+        settings = helpers.get_user_settings()
+        if not settings.notify_update_addon and not OpRemindUpdateAddon.remind_later:
+            OpRemindUpdateAddon.remind_later = True
+        return settings.notify_update_addon and not OpRemindUpdateAddon.remind_later
+
+    def draw(self, context):
+        layout = self.layout
+        rpr = context.scene.rpr
+
+        col = layout.column()
+        must_update = self.json_obj['mustUpdate']
+
+        split = col.split(percentage=0.5)
+        col1 = split.column()
+        split = split.split()
+        col2 = split.column(align=True)
+
+        col1.label('New Version availible:')
+        if must_update:
+            col2.label('Critical update', icon='ERROR')
+        else:
+            col2.label('')
+
+        col1.label('Version: ')
+        col2.label(self.json_obj['version'])
+        col1.label('Date: ')
+        col2.label(self.json_obj['date'])
+
+        col.label(self.json_obj['changes'])
+
+        if OpUpdateAddon.thread:
+            if OpUpdateAddon.thread.is_alive():
+                percent = 0
+                if OpUpdateAddon.thread.total_size:
+                    percent = OpUpdateAddon.thread.downloaded /OpUpdateAddon.thread.total_size * 100.0
+
+                downloaded_mb = OpUpdateAddon.thread.downloaded / (1024 * 1024)
+                s = str('Downloaded: %.1f%% (%.2fmb)' % (percent, downloaded_mb))
+
+                split = col.row().split(percentage=0.66)
+                split.column().label(s)
+                split.column().operator("rpr.op_update_addon", text='Cancel', icon='CANCEL').op = 'cancel'
+            else:
+                if OpUpdateAddon.thread.msi_file_name:
+                    col.operator("rpr.op_update_addon", text='Install').op = 'install'
+                    col.label('Please close Blender after running installer', icon='ERROR')
+                else:
+                    OpUpdateAddon.thread = None
+        else:
+            h = col.operator("rpr.op_update_addon", text='Update Addon')
+            h.op = 'download'
+            if self.json_obj:
+                h.url = self.json_obj['url']
+
+            if must_update and self.is_notify():
+                row = col.row()
+                row.operator("rpr.op_remind", text='Remind me later').op = 'later'
+                #row.operator("rpr.op_remind", text='Never remind').op = 'never'
+
+            settings = helpers.get_user_settings()
+            col.prop(settings, 'notify_update_addon')
+
+
+@rpraddon.register_class
+class OpRemindUpdateAddon(bpy.types.Operator):
+    bl_idname = "rpr.op_remind"
+    bl_label = "Remind Update Addon"
+
+    remind_later = False
+    op = bpy.props.StringProperty()
+
+    def execute(self, context):
+        if self.op == 'later':
+            OpRemindUpdateAddon.remind_later = True
+        return {'FINISHED'}
+
+
+@rpraddon.register_class
+class OpUpdateAddon(bpy.types.Operator):
+    bl_idname = "rpr.op_update_addon"
+    bl_label = "Update Addon"
+
+    thread = None
+
+    op = bpy.props.StringProperty()
+    url = bpy.props.StringProperty()
+
+    def execute(self, context):
+        logging.info('OpUpdateAddon op:', self.op)
+        if self.op == 'download':
+            if not OpUpdateAddon.thread:
+                logging.info('create download thread...')
+                # self.url = '...'
+                OpUpdateAddon.thread = DownloadAddonMsiThread(self.url)
+                OpUpdateAddon.thread.start()
+                #OpUpdateAddon.thread.msi_file_name = '...'
+        elif self.op == 'install':
+            if OpUpdateAddon.thread.msi_file_name:
+                cmd = str('msiexec /i %s' % OpUpdateAddon.thread.msi_file_name)
+                logging.info('cmd: ', cmd)
+                import subprocess
+                subprocess.Popen(cmd)
+        else:
+            if OpUpdateAddon.thread:
+                if OpUpdateAddon.thread.is_alive():
+                    logging.info('Cancel downloading...')
+                    OpUpdateAddon.thread.terminate = True
+
+        return {'FINISHED'}
+
+
