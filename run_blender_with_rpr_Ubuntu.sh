3f3907c015219dadf0daae447a3aae7b16dcce73
Maksym Sditanov <msditanov@200volts.com>
2017-12-08 15:35:39 +0200

    Feature/ubuntu imageprocessing (#22)
    * Ubuntu run script: Appeing LD_LIBRARY_PATH
    
    Attach to LD_LIBRARY_PATH variable directory to required libraries
    
    Previous Ubuntu run script replace LD_LIBRARY_PATH.
    Such behaviour doesn't allow put libraries in different place.
    
    * Add pre-load of rprimagefilter library
    
    * Fix directories for rprimagefilter library
    
    * Ubuntu run script: copy rprimageprocessing to SDK dir
    
    Copy missed dependency if it missed
    
    * Ubuntu: port rprinfo to ubuntu
    
    * Ubuntu: Update python bindings + imageprocessing
    
    Update python bindings to handle newer version of imageprocessing
    library

--- run_blender_with_rpr_Ubuntu.sh
+++ run_blender_with_rpr_Ubuntu.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+set -e
+
+RPR_SDK="ThirdParty/RadeonProRender SDK/Linux-Ubuntu"
+IMAGE_FILTER_DIR="ThirdParty/RadeonProImageProcessing/Linux/Ubuntu"
+IMAGE_FILTER_LIBNAME="libRadeonImageFilters64.so"
+
+function pre_init()
+{
+	# Python scipts waiting, that all dynamic dependecies will be at RadeonSDK directory.
+	# It is not well, as in future RPR can have more dynamic deps and they can be suitied in different
+	# directories
+	
+	if [ ! -f "$RPR_SDK/lib/$IMAGE_FILTER_LIBNAME" ];
+	then
+		cp "$IMAGE_FILTER_DIR/lib64/$IMAGE_FILTER_LIBNAME" "$RPR_SDK/lib/$IMAGE_FILTER_LIBNAME"
+	fi
+}
+
+
 if [ -x "$BLENDER_EXE" ]; then
 
+	pre_init
+	
 	rm -rf dist/
 	mkdir dist
 	cp -r "ThirdParty/RadeonProRender SDK/Linux-Ubuntu/lib" dist/
@@ -12,11 +33,11 @@ if [ -x "$BLENDER_EXE" ]; then
 	CDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 	DIST_LIB="$CDIR/distlib"
 
-	export LD_LIBRARY_PATH="$DIST_LIB"
+	export LD_LIBRARY_PATH="$DIST_LIB:$LD_LIBRARY_PATH"
 
 	python3 tests/commandline/run_blender.py "$BLENDER_EXE" tests/commandline/test_rpr.py
 
-	rm distlib
+#	rm distlib
 
 	exit
 
