d45a593576faa4b9d5ab5bcfca5d33c57db69607
tnarine-amd <tnarine@amd.com>
2017-12-15 11:02:03 -0500

    Minor updates to some scripts and ignore file.

--- ThirdParty/unix_update.sh
+++ ThirdParty/unix_update.sh
@@ -9,6 +9,7 @@ if [ -d "$ThirdPartyDir" ]; then
     rm -rf 'Expat 2.1.0'
     rm -rf OpenCL
     rm -rf OpenColorIO
+    rm -rf 'RadeonProImageProcessing'
     rm -rf 'RadeonProRender SDK'
     rm -rf RadeonProRender-GLTF
     rm -rf ffmpeg
@@ -18,8 +19,20 @@ if [ -d "$ThirdPartyDir" ]; then
     rm -rf oiio-mac
     rm -rf synColor
 
-    cp -r $ThirdPartyDir/* .
-
+    cp -r $ThirdPartyDir/AxfPackage/* AxfPackage
+    cp -r "$ThirdPartyDir/Expat 2.1.0/*" "Expat 2.1.0"
+    cp -r $ThirdPartyDir/OpenCL/* OpenCL
+    cp -r $ThirdPartyDir/OpenColorIO/* OpenColorIO
+    cp -r $ThirdPartyDir/RadeonProImageProcessing/* RadeonProImageProcessing
+    cp -r "$ThirdPartyDir/RadeonProRender SDK/*" "RadeonProRender SDK"
+    cp -r $ThirdPartyDir/RadeonProRender-GLTF/* RadeonProRender-GLTF
+    cp -r $ThirdPartyDir/ffmpeg/* ffmpeg
+    cp -r $ThirdPartyDir/glew/* glew
+    cp -r $ThirdPartyDir/json/* json
+    cp -r $ThirdPartyDir/oiio/* oiio
+    cp -r $ThirdPartyDir/oiio-mac/* oiio-mac
+    cp -r $ThirdPartyDir/synColor/* synColor
+	
     echo ===============================================================
     pushd $ThirdPartyDir 
     git remote update
