34c3566ba6805d782a9381b0c5bfa627c397dccf
tnarine-amd <tracy.narine@amd.com>
2017-12-15 11:36:34 -0500

    Minor script upates for lnx.

--- ThirdParty/unix_update.sh
+++ ThirdParty/unix_update.sh
@@ -19,19 +19,19 @@ if [ -d "$ThirdPartyDir" ]; then
     rm -rf oiio-mac
     rm -rf synColor
 
-    cp -r $ThirdPartyDir/AxfPackage/* AxfPackage
-    cp -r "$ThirdPartyDir/Expat 2.1.0/*" "Expat 2.1.0"
-    cp -r $ThirdPartyDir/OpenCL/* OpenCL
-    cp -r $ThirdPartyDir/OpenColorIO/* OpenColorIO
-    cp -r $ThirdPartyDir/RadeonProImageProcessing/* RadeonProImageProcessing
-    cp -r "$ThirdPartyDir/RadeonProRender SDK/*" "RadeonProRender SDK"
-    cp -r $ThirdPartyDir/RadeonProRender-GLTF/* RadeonProRender-GLTF
-    cp -r $ThirdPartyDir/ffmpeg/* ffmpeg
-    cp -r $ThirdPartyDir/glew/* glew
-    cp -r $ThirdPartyDir/json/* json
-    cp -r $ThirdPartyDir/oiio/* oiio
-    cp -r $ThirdPartyDir/oiio-mac/* oiio-mac
-    cp -r $ThirdPartyDir/synColor/* synColor
+    cp -r $ThirdPartyDir/AxfPackage AxfPackage
+    cp -r "$ThirdPartyDir/Expat 2.1.0" "Expat 2.1.0"
+    cp -r $ThirdPartyDir/OpenCL OpenCL
+    cp -r $ThirdPartyDir/OpenColorIO OpenColorIO
+    cp -r $ThirdPartyDir/RadeonProImageProcessing RadeonProImageProcessing
+    cp -r "$ThirdPartyDir/RadeonProRender SDK" "RadeonProRender SDK"
+    cp -r $ThirdPartyDir/RadeonProRender-GLTF RadeonProRender-GLTF
+    cp -r $ThirdPartyDir/ffmpeg ffmpeg
+    cp -r $ThirdPartyDir/glew glew
+    cp -r $ThirdPartyDir/json json
+    cp -r $ThirdPartyDir/oiio oiio
+    cp -r $ThirdPartyDir/oiio-mac oiio-mac
+    cp -r $ThirdPartyDir/synColor synColor
 	
     echo ===============================================================
     pushd $ThirdPartyDir 
--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-python3 build.py
+cxml="/usr/bin/castxml"
+if [ -f "$cxml" ]; then
+	python3 src/bindings/pyrpr/src/pyrprapi.py $cxml
+	python3 build.py
+else
+	echo Error : $cxml is required for build
+fi
