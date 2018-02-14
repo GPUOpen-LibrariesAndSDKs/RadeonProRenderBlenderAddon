02cc89b8d13f635270e0f3d022dd7414b531b1e4
Denis Kolliakov <den13k@yandex.ru>
2018-02-14 17:38:05 +0300

    Implemented changes for the new versioning system. (#55)
    * Implemented new versioning approach.
    
    * Updated vesrsion and ThirdParty script (windows).
    
    * Added ThirdParty update.
    
    * Updated after review.

--- ThirdParty/win_update.cmd
+++ ThirdParty/win_update.cmd
@@ -1,44 +1,34 @@
+@echo off
 
-echo off
 set ThirdPartyDir="..\..\RadeonProRenderThirdPartyComponents"
 
-IF exist %ThirdPartyDir% ( 
-    echo Updating %ThirdPartyDir% 
-
-    rd /S /Q AxfPackage
-    rd /S /Q 'Expat 2.1.0'
-    rd /S /Q OpenCL
-    rd /S /Q OpenColorIO
-    rd /S /Q 'RadeonProImageProcessing'
+if exist %ThirdPartyDir% ( 
+	if "%1"=="check_thirdparty" (
+		echo Checking %ThirdPartyDir% 
+	    echo ===============================================================
+	    pushd %ThirdPartyDir% 
+    	git remote update
+	    git status -uno
+		popd
+	)
+	
     rd /S /Q 'RadeonProRender SDK'
+    rd /S /Q 'RadeonProImageProcessing'
+    rd /S /Q AxfPackage
     rd /S /Q RadeonProRender-GLTF
-    rd /S /Q ffmpeg
-    rd /S /Q glew
     rd /S /Q json
     rd /S /Q oiio
-    rd /S /Q oiio-mac
-    rd /S /Q synColor
+    rd /S /Q glew
 
-    xcopy /S /Y /I %ThirdPartyDir%\AxfPackage\* AxfPackage
-    xcopy /S /Y /I "%ThirdPartyDir%\Expat 2.1.0\*" "Expat 2.1.0"
-    xcopy /S /Y /I %ThirdPartyDir%\OpenCL\* "OpenCL"
-    xcopy /S /Y /I %ThirdPartyDir%\OpenColorIO\* OpenColorIO
-    xcopy /S /Y /I %ThirdPartyDir%\RadeonProImageProcessing\* RadeonProImageProcessing
     xcopy /S /Y /I "%ThirdPartyDir%\RadeonProRender SDK\*" "RadeonProRender SDK"
+    xcopy /S /Y /I "%ThirdPartyDir%\RadeonProImageProcessing\*" "RadeonProImageProcessing"
+    xcopy /S /Y /I %ThirdPartyDir%\AxfPackage\ReleaseDll\* AxfPackage\ReleaseDll
+    xcopy /S /Y /I %ThirdPartyDir%\AxfPackage\AxfInclude\* AxfPackage\AxfInclude
     xcopy /S /Y /I %ThirdPartyDir%\RadeonProRender-GLTF\* "RadeonProRender-GLTF"
-    xcopy /S /Y /I %ThirdPartyDir%\ffmpeg\* ffmpeg
-    xcopy /S /Y /I %ThirdPartyDir%\glew\* glew
     xcopy /S /Y /I %ThirdPartyDir%\json\* json
     xcopy /S /Y /I %ThirdPartyDir%\oiio\* oiio
-    xcopy /S /Y /I %ThirdPartyDir%\oiio-mac\* oiio-mac
-    xcopy /S /Y /I %ThirdPartyDir%\synColor\* synColor
+    xcopy /S /Y /I %ThirdPartyDir%\glew\* glew
 
-    echo ===============================================================
-    pushd %ThirdPartyDir% 
-    git remote update
-    git status -uno
-    popd
-	
-) ELSE ( 
+) else ( 
     echo Cannot update as %ThirdPartyDir% missing
 )
--- build.cmd
+++ build.cmd
@@ -2,25 +2,31 @@
 
 setlocal enabledelayedexpansion
 
+:cleanup
+rmdir /S /Q RPRBlenderHelper\.build src\bindings\pyrpr\.build src\bindings\pyrpr\src\__pycache__
+
+:update_3dparty
+pushd ThirdParty
+call win_update.cmd
+popd
+
 :check_castxml
+set castxml=..\RadeonProRenderThirdPartyComponents\castxml\win\bin\castxml.exe
 
-set castxml=%1
-
-if "%castxml%"=="" (
-	echo Please set a path to castxml tool as the first parameter.
-	goto end
+if not exist %castxml% (
+	echo Castxml tool is not found.
+	goto :eof
 ) else (
 	echo Path to castxml tool %castxml%
 
-	goto vs_setup
+	goto :vs_setup
 )
 
 :vs_setup
-
 call vs_path.bat
 
 :: VS not detected
-if %vs_ver%=="" goto vs_error
+if %vs_ver%=="" goto :vs_error
 
 :: check VS version
 set vs_major=%vs_ver:~0,2%
@@ -28,7 +34,7 @@ set vs_major=%vs_ver:~0,2%
 if %vs_major%==14 (
 	echo Visual Studio 2015 is installed.
 	
-	goto build_plugin
+	goto :build_plugin
 )
 
 set vs17=""
@@ -45,18 +51,14 @@ if %vs_major%==15 (
 	call !vs17! amd64 -vcvars_ver=14.0
 	popd
 
-	goto build_plugin
+	goto :build_plugin
 )
 
 :vs_error
 	echo Visual Studio 2015 or newer has to be installed.
 	echo Newer version of Visual Studio will be used if it's present (v140 toolset has to be installed).
-	goto end
+	goto :eof
 
 :build_plugin
-
 py -3.5 src\bindings\pyrpr\src\pyrprapi.py %castxml%
-	
 py -3.5 build.py
-
-:end
