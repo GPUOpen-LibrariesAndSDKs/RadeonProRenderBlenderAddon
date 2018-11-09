1746ec98b13c5fdae657b287af20384120899c0e
tnarine-amd <tracy.narine@amd.com>
2018-11-09 12:54:25 -0500

    Dev/update/thirdparty cleanup (#192)
    * Removed unnecessary third party libraries.
    
    * Removing unnecessary libraries on macos.

--- ThirdParty/unix_update.sh
+++ ThirdParty/unix_update.sh
@@ -5,33 +5,13 @@ ThirdPartyDir="../../RadeonProRenderThirdPartyComponents"
 if [ -d "$ThirdPartyDir" ]; then
     echo Updating $ThirdPartyDir
 
-    rm -rf AxfPackage
-    rm -rf 'Expat 2.1.0'
-    rm -rf OpenCL
-    rm -rf OpenColorIO
     rm -rf 'RadeonProImageProcessing'
     rm -rf 'RadeonProRender SDK'
     rm -rf RadeonProRender-GLTF
-    rm -rf ffmpeg
-    rm -rf glew
-    rm -rf json
-    rm -rf oiio
-    rm -rf oiio-mac
-    rm -rf synColor
 
-    cp -r $ThirdPartyDir/AxfPackage AxfPackage
-    cp -r "$ThirdPartyDir/Expat 2.1.0" "Expat 2.1.0"
-    cp -r $ThirdPartyDir/OpenCL OpenCL
-    cp -r $ThirdPartyDir/OpenColorIO OpenColorIO
     cp -r $ThirdPartyDir/RadeonProImageProcessing RadeonProImageProcessing
     cp -r "$ThirdPartyDir/RadeonProRender SDK" "RadeonProRender SDK"
     cp -r $ThirdPartyDir/RadeonProRender-GLTF RadeonProRender-GLTF
-    cp -r $ThirdPartyDir/ffmpeg ffmpeg
-    cp -r $ThirdPartyDir/glew glew
-    cp -r $ThirdPartyDir/json json
-    cp -r $ThirdPartyDir/oiio oiio
-    cp -r $ThirdPartyDir/oiio-mac oiio-mac
-    cp -r $ThirdPartyDir/synColor synColor
 	
     echo ===============================================================
     pushd $ThirdPartyDir 
