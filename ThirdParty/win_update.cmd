bc6c3b037f132219355ed5feab6d447084dbb8cf
tnarine-amd <tracy.narine@amd.com>
2018-11-09 12:54:25 -0500

    Dev/update/thirdparty cleanup (#192)
    * Removed unnecessary third party libraries.
    
    * Removing unnecessary libraries on macos.

--- ThirdParty/win_update.cmd
+++ ThirdParty/win_update.cmd
@@ -14,19 +14,12 @@ if exist %ThirdPartyDir% (
 	
     rd /S /Q "RadeonProRender SDK"
     rd /S /Q RadeonProImageProcessing
-    rd /S /Q AxfPackage
     rd /S /Q RadeonProRender-GLTF
-    rd /S /Q json
-    rd /S /Q oiio
     rd /S /Q glew
 
     xcopy /S /Y /I "%ThirdPartyDir%\RadeonProRender SDK\*" "RadeonProRender SDK"
     xcopy /S /Y /I %ThirdPartyDir%\RadeonProImageProcessing\* RadeonProImageProcessing
-    xcopy /S /Y /I %ThirdPartyDir%\AxfPackage\ReleaseDll\* AxfPackage\ReleaseDll
-    xcopy /S /Y /I %ThirdPartyDir%\AxfPackage\AxfInclude\* AxfPackage\AxfInclude
     xcopy /S /Y /I %ThirdPartyDir%\RadeonProRender-GLTF\* RadeonProRender-GLTF
-    xcopy /S /Y /I %ThirdPartyDir%\json\* json
-    xcopy /S /Y /I %ThirdPartyDir%\oiio\* oiio
     xcopy /S /Y /I %ThirdPartyDir%\glew\* glew
 
 ) else ( 
