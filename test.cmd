263934ca00068e71e736d8bd7727e4ec7389c2cc
bnagirniak <BNagirniak@luxoft.com>
2018-12-04 10:28:48 +0200

    Removed old version of addon for Blender 2.79

--- test.cmd
+++
@@ -1,8 +0,0 @@
-echo off
-if ""=="%BLENDER_EXE%" goto error 
-
-py -3.5 test.py
-exit
-
-:error
-echo "Please set BLENDER_EXE environment variable"
--- tests/cython/gl/build.cmd
+++
@@ -1,1 +0,0 @@
-py -3 setup.py build_ext --inplace
