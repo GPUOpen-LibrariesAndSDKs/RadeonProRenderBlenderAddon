ad1529c6491e43e2b3f321c23aabf8460b143940
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2018-12-06 12:03:20 +0300

    Add basic output settings

--- src/rprblender/properties/Object.py
+++ src/rprblender/properties/Object.py
@@ -1,4 +1,4 @@
-from .base import PropertyBase, PanelBase
+from .base import RPR_Property, RPR_Panel
 import bpy
 from bpy.props import (
     BoolProperty,
@@ -10,11 +10,21 @@ from bpy.props import (
 )
 import pyrpr
 
+from rprblender import logging
 
-class RPR_ObjectProperties(PropertyBase):
-    ''' Properties for objects '''
+
+class RPR_ObjectProperties(RPR_Property):
+    """
+    Properties for objects
+    """
 
     camera_visible: BoolProperty(name='Camera Visibility', default=True)
+
+    shadowcatcher: BoolProperty(
+        name="Shadow Catcher",
+        description="Use this object as shadowcatcher",
+        default=False,
+    )
 
     def sync(self, parent: bpy.types.Object, context: pyrpr.Context):
         ''' sync the object and any data attached '''
@@ -26,20 +36,25 @@ class RPR_ObjectProperties(PropertyBase):
 
     @classmethod
     def register(cls):
+        logging.info("register", tag='Object')
         bpy.types.Object.rpr = PointerProperty(
             name="RPR Object Settings",
-            description="RPR object settings",
+            description="RPR Object settings",
             type=cls,
         )
 
     @classmethod
     def unregister(cls):
+        logging.info("unregister", tag='Object')
         del bpy.types.Object.rpr
 
 
-class RPR_OBJECT_PT_object(PanelBase):
-    ''' panel to display above properties '''
-    bl_idname = "object_PT_property_example"
+class RPR_OBJECT_PT_object(RPR_Panel, bpy.types.Panel):
+    """
+    panel to display above properties
+    """
+
+    bl_idname = "rpr_object_PT_properties"
     bl_label = "RPR Settings"
     bl_context = 'object'
 
@@ -52,7 +67,7 @@ class RPR_OBJECT_PT_object(PanelBase):
             rpr = getattr(context.object, 'rpr', None)
             self.layout.row().label(text="Just the test label")
             if rpr:
-                self.layout.row().prop(rpr, "camera_visible")
+                self.layout.row().prop(rpr, 'camera_visible')
 
 
 classes = (RPR_ObjectProperties, RPR_OBJECT_PT_object)
--- src/rprblender/properties/base.py
+++ src/rprblender/properties/base.py
@@ -1,30 +1,31 @@
 ''' property classes should be self contained.  They may include:
-	PropertyGroup class
-		with properties that can be attached to a blender ID type
-		methods for syncing these properties
-	And panel classes for displaying these properties
+    PropertyGroup class
+        with properties that can be attached to a blender ID type
+        methods for syncing these properties
+    And panel classes for displaying these properties
 
-	The idea here is to keep all the properties syncing, data, display etc in one place.  
-	Basically a "model/view" type pattern where we bring them together for ease of maintenance.  
-	Slightly inspired by vue.js
+    The idea here is to keep all the properties syncing, data, display etc in one place.
+    Basically a "model/view" type pattern where we bring them together for ease of maintenance.
+    Slightly inspired by vue.js
 
-	TODO could we use decorators to register???
+    TODO could we use decorators to register???
 '''
 
 import bpy
 import pyrpr
+from abc import ABCMeta, abstractmethod
 
 
-class PropertyBase(bpy.types.PropertyGroup):
-	def sync(self, parent, context: pyrpr.Context):
-		''' Sync will update this object in the context.  
-			And call any sub-objects that need to be synced  
-			rpr_context object in the binding will be the only place we keep 
-			"lists of items synced." '''
-		pass
+class RPR_Property(bpy.types.PropertyGroup):
+    def sync(self, parent, context: pyrpr.Context):
+        ''' Sync will update this object in the context.
+            And call any sub-objects that need to be synced
+            rpr_context object in the binding will be the only place we keep
+        "lists of items synced." '''
+        pass
 
 
-class PanelBase(bpy.types.Panel):
+class RPR_Panel:
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "render"
