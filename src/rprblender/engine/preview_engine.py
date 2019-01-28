d544e91fd2d6e5754f33df7c1bf2f9667ced0564
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2019-01-28 17:45:55 +0200

    RPRBLND-719: Refactoring: move render and view_layer syncing to engines. (#228)
    * RPRBLND-719: Refactoring: move render and view_layer syncing to engines.
    Done.
    
    * Reverted back properties/render.py, properties/view_layer.py, engine/engine.py.
    Splitted init() and resize() in rpr_context - requires for render engines.
    Made code to be corresponded to design doc.

--- src/rprblender/engine/preview_engine.py
+++ src/rprblender/engine/preview_engine.py
@@ -38,7 +38,11 @@ class PreviewEngine(Engine):
         log('Start syncing')
         self.is_synced = False
 
-        self._sync_render(depsgraph.scene)
+        scene = depsgraph.scene
+        settings_scene = bpy.context.scene
+
+        settings_scene.rpr.sync(self.rpr_context)
+        self.rpr_context.resize(scene.render.resolution_x, scene.render.resolution_y)
 
         # getting visible objects
         for i, obj_instance in enumerate(depsgraph.object_instances):
@@ -48,46 +52,14 @@ class PreviewEngine(Engine):
             except SyncError as e:
                 log.warn(e, "Skipping")
 
+        self.rpr_context.scene.set_name(scene.name)
         self.rpr_context.scene.set_camera(self.rpr_context.objects[utils.key(depsgraph.scene.camera)])
         self.rpr_context.enable_aov(pyrpr.AOV_COLOR)
         self.rpr_context.enable_aov(pyrpr.AOV_DEPTH)
 
         self.rpr_context.set_parameter('preview', False)
 
+        self.render_iterations = settings_scene.rpr.viewport_limits.iterations
+
         self.is_synced = True
         log('Finish sync')
-
-    def _sync_render(self, scene):
-        log("sync_render", scene)
-
-        rpr = bpy.context.scene.rpr     # getting rpr settings from user's scene
-
-        context_flags = 0
-        context_props = []
-        if rpr.devices in ['CPU', 'GPU+CPU']:
-            context_flags |= pyrpr.Context.cpu_device['flag']
-            context_props.extend([pyrpr.CONTEXT_CREATEPROP_CPU_THREAD_LIMIT, rpr.cpu_threads])
-        if rpr.devices in ['GPU', 'GPU+CPU']:
-            for i, gpu_state in enumerate(rpr.gpu_states):
-                if gpu_state:
-                    context_flags |= pyrpr.Context.gpu_devices[i]['flag']
-
-        width = int(scene.render.resolution_x * scene.render.resolution_percentage / 100)
-        height = int(scene.render.resolution_y * scene.render.resolution_percentage / 100)
-
-        context_props.append(0) # should be followed by 0
-        self.rpr_context.init(width, height, context_flags, context_props)
-        self.rpr_context.scene.set_name(scene.name)
-
-        # set light paths values
-        self.rpr_context.set_parameter('maxRecursion', rpr.max_ray_depth)
-        self.rpr_context.set_parameter('maxdepth.diffuse', rpr.diffuse_depth)
-        self.rpr_context.set_parameter('maxdepth.glossy', rpr.glossy_depth)
-        self.rpr_context.set_parameter('maxdepth.shadow', rpr.shadow_depth)
-        self.rpr_context.set_parameter('maxdepth.refraction', rpr.refraction_depth)
-        self.rpr_context.set_parameter('maxdepth.refraction.glossy', rpr.glossy_refraction_depth)
-        self.rpr_context.set_parameter('radianceclamp', rpr.clamp_radiance if rpr.use_clamp_radiance else np.finfo(np.float32).max)
-
-        self.rpr_context.set_parameter('raycastepsilon', rpr.ray_cast_epsilon * 0.001) # Convert millimeters to meters
-
-        self.render_iterations = rpr.viewport_limits.iterations
