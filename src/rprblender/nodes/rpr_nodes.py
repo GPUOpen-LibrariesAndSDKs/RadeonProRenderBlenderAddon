a3dcdbfb7153202a93321f9f42601fd5304f0d04
bsavery <brian.savery@gmail.com>
2019-02-14 19:21:00 -0800

    Finish Uber implementation.

--- src/rprblender/nodes/rpr_nodes.py
+++ src/rprblender/nodes/rpr_nodes.py
@@ -56,10 +56,10 @@ class RPRShadingNodeUber(RPRShadingNode):
         'Reflection Weight': ('rpr_socket_weight_soft', 1.0, 'RPRX_UBER_MATERIAL_REFLECTION_WEIGHT', 'self.enable_reflection'),
         'Reflection Color': ('rpr_socket_color', (1.0, 1.0, 1.0, 1.0), 'RPRX_UBER_MATERIAL_REFLECTION_COLOR', 'self.enable_reflection'),
         'Reflection Roughness': ('rpr_socket_weight', 0.25, 'RPRX_UBER_MATERIAL_REFLECTION_ROUGHNESS', 'self.enable_reflection'),
-        'Reflection Anisotropy': ('rpr_socket_float_min1_max1', 0.0, 'RPRX_UBER_MATERIAL_REFLECTION_ANISOTROPY', 'self.enable_reflection'),
-        'Reflection Anisotropy Rotation': ('rpr_socket_angle360', 0.0, 'RPRX_UBER_MATERIAL_REFLECTION_ANISOTROPY_ROTATION', 'self.enable_reflection'),
         'Reflection IOR': ('rpr_socket_ior', 1.5, 'RPRX_UBER_MATERIAL_REFLECTION_IOR', "self.enable_reflection and self.reflection_mode == 'RPRX_UBER_MATERIAL_REFLECTION_MODE_PBR'"),
         'Reflection Metalness': ('rpr_socket_weight', 0.0, 'RPRX_UBER_MATERIAL_REFLECTION_METALNESS', "self.enable_reflection and self.reflection_mode == 'RPRX_UBER_MATERIAL_REFLECTION_MODE_METALNESS'"),
+        'Reflection Anisotropy': ('rpr_socket_float_min1_max1', 0.0, 'RPRX_UBER_MATERIAL_REFLECTION_ANISOTROPY', 'self.enable_reflection'),
+        'Reflection Anisotropy Rotation': ('rpr_socket_angle360', 0.0, 'RPRX_UBER_MATERIAL_REFLECTION_ANISOTROPY_ROTATION', 'self.enable_reflection'),
         'Reflection Normal': ('rpr_socket_link', None, 'RPRX_UBER_MATERIAL_REFLECTION_NORMAL', 'self.enable_reflection and not self.reflection_use_shader_normal'),
         
         'Refraction Weight': ('rpr_socket_weight_soft', 1.0, 'RPRX_UBER_MATERIAL_REFRACTION_WEIGHT', 'self.enable_refraction'),
@@ -99,11 +99,9 @@ class RPRShadingNodeUber(RPRShadingNode):
     }
 
     def update_visibility(self, context):
-        ''' update visibility of list of sockets based on enabled properties '''
+        ''' update visibility of each in list of sockets based on enabled properties '''
         for socket_name, socket in self.inputs.items():
             socket_type, socket_default, rpr_name, eval_string = self.node_sockets[socket_name]
-
-            print(socket_name, eval_string)
 
             # eval the socket enable string
             socket.enabled = eval(eval_string)
@@ -111,7 +109,7 @@ class RPRShadingNodeUber(RPRShadingNode):
 
     enable_diffuse: BoolProperty(name="Diffuse", description="Enable Diffuse", default=True, update=update_visibility)
     diffuse_use_shader_normal: BoolProperty(name="Diffuse use shader normal", description="Use the master shader normal (disable to override)", default=True, update=update_visibility)
-    enable_backscattering: BoolProperty(name="Diffuse", description="Enable Diffuse", default=True, update=update_visibility)
+    enable_backscattering: BoolProperty(name="Backscattering", description="Enable Backscattering", default=False, update=update_visibility)
 
     enable_reflection: BoolProperty(name="Reflection", description="Enable Reflection", default=True, update=update_visibility)
     reflection_use_shader_normal: BoolProperty(name="Reflection use shader normal", description="Use the master shader normal (disable to override)", default=True, update=update_visibility)
@@ -160,36 +158,68 @@ class RPRShadingNodeUber(RPRShadingNode):
         # save self as blender_node
         self.blender_node = self
 
+
     def export(self, socket, material_exporter):
-        ''' export based on rules '''
-        # todo handle special cases. 
+        ''' export sockets to the uber param specced in self.node_sockets '''
         self.material_exporter = material_exporter
         uber_node = material_exporter.create_rpr_node('RPRX_MATERIAL_UBER', material_exporter.get_node_key(self, 'Shader'))
 
-        # temp set to metalness mode
-        uber_node.set_input(get_rpr_val("RPRX_UBER_MATERIAL_REFLECTION_MODE"), get_rpr_val("RPRX_UBER_MATERIAL_REFLECTION_MODE_METALNESS"))
+        shader_normal_val = get_node_value(material_exporter, self, 'Normal') if self.inputs['Normal'].is_linked else None
 
         for socket_name, socket_desc in self.node_sockets.items():
             socket_type, socket_default, rpr_name, eval_string = socket_desc
 
-
+            # only set the param if enabled
             if rpr_name and eval(eval_string):
                 val = get_node_value(material_exporter, self, socket_name)
-                print(lobe_name, socket_name, val, rpr_name)
-                if val is not None and 'Normal' not in socket_name and 'Displacement' not in socket_name:
-                    uber_node.set_input(get_rpr_val(rpr_name), val)
+                if val is not None:
+                    # if this normal and is unlinked skip
+                    if 'Normal' in socket_name and not self.inputs[socket_name].is_linked:
+                        continue
+                    else:
+                        uber_node.set_input(get_rpr_val(rpr_name), val)
 
+
+            # if normal is not enabled, set to the shader normal
+            elif rpr_name and 'Normal' in socket_name and shader_normal_val:
+                uber_node.set_input(get_rpr_val(rpr_name), shader_normal_val)
+            
             else:
+                # if this is a weight and not enabled, set to 0
                 if "Weight" in socket_name:
-                    uber_node.set_input(get_rpr_val(rpr_val), 0.0)
+                    uber_node.set_input(get_rpr_val(rpr_name), 0.0)
 
+        # set reflection mode
+        uber_node.set_input(get_rpr_val('RPRX_UBER_MATERIAL_REFLECTION_MODE'), get_rpr_val(self.reflection_mode))
+        
         return uber_node
 
-    def draw_buttons(self, context, layout):
-        grid = layout.grid_flow(row_major=True, columns=2, align=True)
-        grid.prop(self, 'enable_diffuse', toggle=True)
-        grid.prop(self, 'enable_reflection', toggle=True)
-        grid.prop(self, 'enable_coating', toggle=True)
-        grid.prop(self, 'enable_sss', toggle=True)
 
-        layout.prop(self, 'reflection_mode')
+    def draw_buttons(self, context, layout):
+        col = layout.column(align=True)
+        col.prop(self, 'enable_diffuse', toggle=True)
+        if self.enable_diffuse:
+            box = col.box()
+            box.prop(self, 'diffuse_use_shader_normal')
+            box.prop(self, 'enable_backscattering', toggle=True)
+        
+        col.prop(self, 'enable_reflection', toggle=True)
+        if self.enable_reflection:
+            box = col.box()
+            box.prop(self, 'reflection_use_shader_normal')
+            box.prop(self, 'reflection_mode')
+
+        col.prop(self, 'enable_coating', toggle=True)
+        if self.enable_reflection:
+            box = col.box()
+            box.prop(self, 'coating_use_shader_normal')
+        
+        col.prop(self, 'enable_sheen', toggle=True)
+        col.prop(self, 'enable_emission', toggle=True)
+        col.prop(self, 'enable_sss', toggle=True)
+        col.prop(self, 'enable_normal', toggle=True)
+        col.prop(self, 'enable_transparency', toggle=True)
+        # Don't enable displacements yet
+        #col.prop(self, 'enable_displacement', toggle=True)
+
+            
