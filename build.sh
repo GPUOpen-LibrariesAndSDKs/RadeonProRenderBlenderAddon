6a6843231b3b1ea27a85f1fe8908dd54fd118f4e
tnarine-amd <tracy.narine@amd.com>
2019-03-04 14:21:17 -0500

    A few fixes for building and running on Ubuntu.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 cxml="/usr/bin/castxml"
 if [ -f "$cxml" ]; then
-	python3 src/bindings/pyrpr/src/pyrprapi.py $cxml
+	python3.7 src/bindings/pyrpr/src/pyrprapi.py $cxml
 	if [ -f "./bindings-ok" ]; then
-		python3 build.py
+		python3.7 build.py
 	else
 		echo Compiling bindings failed
 	fi
