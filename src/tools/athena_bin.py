4a67c4c1210f613233a4e3cc7059f7892f2678f0
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2019-06-29 01:59:22 +0300

    RPRBLND-955: Obfuscate athena send code and hide keys from source (#352)
    * RPRBLND-955: Obfuscate athena send code and hide keys from source.
    Created athena_decrypt.py + do_athena_encrypt.py in src/tools.
    Updated athena.py to work with athena.encrypt.
    
    * Added call do_athena_encrypt.py into build.py
    
    * Renamed: athena_decrypt -> athena_bin.py, do_athena_encrypt.py -> encrypt_athena_bin.py.
    athena_bin.py is encrypted into athena.bin

---
+++ src/tools/athena_bin.py
@@ -0,0 +1,7 @@
+import boto3
+import base64
+
+key = base64.standard_b64decode(b'QUtJQUpES0dCTkxFNlo2UUJWUEEvM3BJaWx1RGgweVJZa2lKc21rdm0rS3RscHJxcDRDUVNpNDFuendqYW1kLWF0aGVuYS1wcm9yZW5kZXI=')
+key = key.decode('utf-8')
+client = boto3.client('s3', aws_access_key_id=key[:20], aws_secret_access_key=key[20:60])
+client.upload_file(str(file), key[60:], file.name[:-5])
