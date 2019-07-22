2b1c4a7a7d6664fc9b628943c34ea320b863e18f
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2019-07-22 16:13:11 +0300

    Use subsurface color for backscatter color (#359)

--- src/rprblender/nodes/rpr_nodes.py
+++ src/rprblender/nodes/rpr_nodes.py
@@ -411,7 +411,7 @@ class RPRShaderNodeUber(RPRShaderNode):
                 sss_direction = self.get_input_value('Subsurface Direction')
 
                 rpr_node.set_input(pyrprx.UBER_MATERIAL_BACKSCATTER_WEIGHT, sss_weight)
-                rpr_node.set_input(pyrprx.UBER_MATERIAL_BACKSCATTER_COLOR, (1.0, 1.0, 1.0))
+                rpr_node.set_input(pyrprx.UBER_MATERIAL_BACKSCATTER_COLOR, sss_color)
                 rpr_node.set_input(pyrprx.UBER_MATERIAL_SSS_WEIGHT, sss_weight)
                 rpr_node.set_input(pyrprx.UBER_MATERIAL_SSS_SCATTER_COLOR, sss_color)
 
