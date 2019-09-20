c18caa078f54e59ba028cbfd6c9c0e4f567d07c1
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2019-09-20 19:51:42 +0300

    [RPRBLND-798] Repair RPR export (#391)
    The exporter was broken when two objects and instances iteration methods where turned from static to regular class methods.
    These methods aren't using any class fields, so they can be reverted back to static state.
    
    Changes made:
    
    depsgraph_objects and depsgraph_instances methods of engine/engine.Engine class turned static once again.
    renamed exporter file and class, separate scene sync and file export methods.

--- src/rprblender/operators/export_scene.py
+++ src/rprblender/operators/export_scene.py
@@ -6,22 +6,9 @@ import time
 from bpy.props import StringProperty, BoolProperty, IntProperty
 
 import bpy
-from rprblender.export import (
-    camera,
-    instance,
-    object,
-    particle,
-    world,
-    mesh,
-    light,
-    to_mesh,
-    volume,
-)
 from bpy_extras.io_utils import ExportHelper
-from rprblender.engine.engine import Engine
-from rprblender.engine.context import RPRContext
+from rprblender.engine.export_engine import ExportEngine
 
-import pyrpr_load_store
 from . import RPR_Operator
 
 from rprblender.utils.logging import Log
@@ -75,7 +62,10 @@ class RPR_EXPORT_OP_export_rpr_scene(RPR_Operator, ExportHelper):
             for i in range(self.start_frame, self.end_frame + 1):
                 filepath_frame = "{}.{:04}.{}".format(begin, i, end)
                 scene.frame_set(i)
-                export_rpr_scene(context, filepath_frame)
+
+                exporter = ExportEngine()
+                exporter.sync(context)
+                exporter.export_to_rpr(self.filepath)
                 log.info(f"Finished frame {i} export to '{filepath_frame}'")
 
             scene.frame_set(orig_frame)
@@ -84,49 +74,11 @@ class RPR_EXPORT_OP_export_rpr_scene(RPR_Operator, ExportHelper):
             log.info(f"Starting scene '{scene.name}' RPR export to '{self.filepath}'")
             time_started = time.time()
 
-            export_rpr_scene(context, self.filepath)
+            exporter = ExportEngine()
+            exporter.sync(context)
+            exporter.export_to_rpr(self.filepath)
 
         log.info(f"Finished RPR export in {time.time() - time_started} s")
 
         return {'FINISHED'}
 
-
-def export_rpr_scene(context, filepath):
-    volumes_data = []  # to keep volumes data references until export is finished
-    depsgraph = context.evaluated_depsgraph_get()
-    scene = depsgraph.scene
-
-    rpr_context = RPRContext()
-
-    scene.rpr.init_rpr_context(rpr_context)
-
-    rpr_context.scene.set_name(scene.name)
-    rpr_context.width = int(scene.render.resolution_x * scene.render.resolution_percentage / 100)
-    rpr_context.height = int(scene.render.resolution_y * scene.render.resolution_percentage / 100)
-
-    world.sync(rpr_context, scene.world)
-
-    # camera, objects, particles
-    for obj in Engine.depsgraph_objects(depsgraph, with_camera=True):
-        object.sync(rpr_context, obj)
-
-        for particle_system in obj.particle_systems:
-            particle.sync(rpr_context, particle_system, obj)
-
-    # instances
-    for inst in Engine.depsgraph_instances(depsgraph):
-        instance.sync(rpr_context, inst)
-
-    # rpr_context parameters
-    rpr_context.set_parameter('preview', False)
-    scene.rpr.export_ray_depth(rpr_context)
-    scene.rpr.export_pixel_filter(rpr_context)
-
-    rpr_context.sync_portal_lights()
-
-    # Exported scene will be rendered vertically flipped, flip it back
-    rpr_context.set_parameter('yflip', True)
-
-    pyrpr_load_store.export(filepath, rpr_context.context, rpr_context.scene)
-
-    return {'FINISHED'}
