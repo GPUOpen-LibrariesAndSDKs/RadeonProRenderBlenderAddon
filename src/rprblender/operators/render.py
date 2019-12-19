3c6a4c8e374ffd669884ef3e7f3b85b1adcb4043
bsavery <brian.savery@gmail.com>
2019-12-19 09:17:18 -0800

    Enable Blender built in denoiser on macOS. (#471)
    EFFECT OF CHANGE
    On macOS with blender 2.81, the denoiser uses the blender built-in denoiser. This can be enabled in the View Layer properties, with the button marked "Enable Blender Denoiser". The actual denoiser library is the same as the RPR denoiser for CPU, this just more tightly integrates with the Blender system. Other OS's use the RPR GPU denoiser (for speed), but there is no GPU AI denoiser on macOS.

--- src/rprblender/operators/render.py
+++ src/rprblender/operators/render.py
@@ -31,3 +31,43 @@ class RPR_RENDER_OP_open_web_page(RPR_Operator):
 
         webbrowser.open(url)
         return {'FINISHED'}
+
+class RPR_RENDER_OP_add_denoiser_node(RPR_Operator):
+    '''
+    Operator to add blender denoiser compositor node
+    also enables needed AOVs
+    '''
+
+    bl_idname = "rpr.add_denoiser_node"
+    bl_label = "Add Compositor Denoise Node"
+    bl_description = "Adds a denoiser node in compositor and hooks up for RPR."
+
+    def execute(self, context):
+        # enable use_nodes compositing if not already
+        bpy.context.scene.use_nodes = True
+        nt = bpy.context.scene.node_tree
+        view_layer = context.view_layer
+
+        # add compositor node
+        denoiser_node = nt.nodes.new(type="CompositorNodeDenoise")
+
+        # adds nescessary AOVS
+        view_layer.rpr.enable_aov_by_name('Shading Normal')
+        view_layer.rpr.enable_aov_by_name('Diffuse Albedo')
+
+        # find render output node
+        output_node = next((node for node in nt.nodes if isinstance(node, bpy.types.CompositorNodeComposite)), None)
+
+        # find render result node
+        render_node = next((node for node in nt.nodes if isinstance(node, bpy.types.CompositorNodeRLayers)), None)
+
+        # hook up nodes
+        if output_node is None or render_node is None:
+            return {'FINISHED'}
+        nt.links.new(render_node.outputs['Image'], denoiser_node.inputs['Image'])
+        nt.links.new(render_node.outputs['Shading Normal'], denoiser_node.inputs['Normal'])
+        nt.links.new(render_node.outputs['Diffuse Albedo'], denoiser_node.inputs['Albedo'])
+
+        nt.links.new(denoiser_node.outputs['Image'], output_node.inputs['Image'])
+
+        return {'FINISHED'}
