b48b0cb85949c94a1a4e17b60014eda8c2fac57f
bsavery <brian.savery@gmail.com>
2019-12-19 09:17:18 -0800

    Enable Blender built in denoiser on macOS. (#471)
    EFFECT OF CHANGE
    On macOS with blender 2.81, the denoiser uses the blender built-in denoiser. This can be enabled in the View Layer properties, with the button marked "Enable Blender Denoiser". The actual denoiser library is the same as the RPR denoiser for CPU, this just more tightly integrates with the Blender system. Other OS's use the RPR GPU denoiser (for speed), but there is no GPU AI denoiser on macOS.

--- src/rprblender/ui/view_layer.py
+++ src/rprblender/ui/view_layer.py
@@ -1,7 +1,7 @@
 import pyrpr
 
 from . import RPR_Panel
-
+from rprblender import utils
 
 class RPR_VIEWLAYER_PT_aovs(RPR_Panel):
     bl_label = "RPR Passes"
@@ -33,39 +33,46 @@ class RPR_RENDER_PT_denoiser(RPR_Panel):
         self.layout.prop(context.view_layer.rpr.denoiser, 'enable', text="")
 
     def draw(self, context):
-        self.layout.use_property_split = True
-        self.layout.use_property_decorate = False
-
-        denoiser = context.view_layer.rpr.denoiser
-
-        col = self.layout.column()
-        col.enabled = denoiser.enable
-        col.active = denoiser.is_available(context.scene)
-        col.prop(denoiser, 'filter_type')
-
-        if denoiser.filter_type == 'BILATERAL':
-            col.prop(denoiser, "radius")
-            col.prop(denoiser, 'color_sigma', slider=True)
-            col.prop(denoiser, 'normal_sigma', slider=True)
-            col.prop(denoiser, 'p_sigma', slider=True)
-            col.prop(denoiser, 'trans_sigma', slider=True)
-
-        elif denoiser.filter_type == 'EAW':
-            col.prop(denoiser, 'color_sigma', slider=True)
-            col.prop(denoiser, 'normal_sigma', slider=True)
-            col.prop(denoiser, 'depth_sigma', slider=True)
-            col.prop(denoiser, 'trans_sigma', slider=True)
-
-        elif denoiser.filter_type == 'LWR':
-            col.prop(denoiser, 'samples', slider=True)
-            col.prop(denoiser, 'half_window', slider=True)
-            col.prop(denoiser, 'bandwidth', slider=True)
-
-        elif denoiser.filter_type == 'ML':
-            col.prop(denoiser, 'ml_color_only')
-
+        ''' if this is macOS and 2.81+ use the builtin Blender denoiser '''
+        if utils.USE_BLENDER_DENOISER:
+            if utils.has_denoise_node():
+                self.layout.label(text="Denoiser activated in compositor.")
+            else:
+                self.layout.operator('rpr.add_denoiser_node', text="Enable Blender Denoiser")
         else:
-            raise TypeError("No such filter type: %s" % denoiser.filter_type)
+            self.layout.use_property_split = True
+            self.layout.use_property_decorate = False
+
+            denoiser = context.view_layer.rpr.denoiser
+
+            col = self.layout.column()
+            col.enabled = denoiser.enable
+            col.active = denoiser.is_available(context.scene)
+            col.prop(denoiser, 'filter_type')
+
+            if denoiser.filter_type == 'BILATERAL':
+                col.prop(denoiser, "radius")
+                col.prop(denoiser, 'color_sigma', slider=True)
+                col.prop(denoiser, 'normal_sigma', slider=True)
+                col.prop(denoiser, 'p_sigma', slider=True)
+                col.prop(denoiser, 'trans_sigma', slider=True)
+
+            elif denoiser.filter_type == 'EAW':
+                col.prop(denoiser, 'color_sigma', slider=True)
+                col.prop(denoiser, 'normal_sigma', slider=True)
+                col.prop(denoiser, 'depth_sigma', slider=True)
+                col.prop(denoiser, 'trans_sigma', slider=True)
+
+            elif denoiser.filter_type == 'LWR':
+                col.prop(denoiser, 'samples', slider=True)
+                col.prop(denoiser, 'half_window', slider=True)
+                col.prop(denoiser, 'bandwidth', slider=True)
+
+            elif denoiser.filter_type == 'ML':
+                col.prop(denoiser, 'ml_color_only')
+
+            else:
+                raise TypeError("No such filter type: %s" % denoiser.filter_type)
 
 
 class RPR_RENDER_PT_override(RPR_Panel):
