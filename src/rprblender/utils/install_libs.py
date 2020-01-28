320248123585caa332c17e54883526350cb5858e
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-01-28 17:57:55 +0300

    Wrap subprocess calls by exception catching and logging

--- src/rprblender/utils/install_libs.py
+++ src/rprblender/utils/install_libs.py
@@ -3,24 +3,42 @@ from rprblender.utils.logging import Log
 log = Log(tag="install_libs")
 
 
+def run_pip(*args):
+    """ Run pip install with current user access level """
+    import bpy
+    import subprocess
+
+    log(f"Running the subprocess.call 'py -m pip install {args} --user'")
+
+    try:
+        res = subprocess.call([bpy.app.binary_path_python, "-m", "pip", "install", *args, "--user"])
+        # note: success retcode is 0. No info on where the lib was installed
+        return True
+    # catch any exception, for debug
+    except Exception as e:
+        log.warn(f"Subprocess call failed: {e}")
+
+    return False
+
+
 def ensure_boto3() -> None:
     """
     Try to install boto3 library at the addon launch time
     """
     # use this snippet to install boto3 library with all the dependencies if absent at the addon launch time
     # note: still it will be available at the next Blender launch only
-    # TODO: check if scene reload works as well (note: even then it couldn't be used at all; just to be sure on how it works)
     try:
         import boto3
     except ImportError:
         log.info("Installing boto3 library...")
-        import bpy
-        import subprocess
-        import importlib
-        # subprocess.call([bpy.app.binary_path_python, "-m", "ensurepip"])  # seems to be working fine without it
-        subprocess.call([bpy.app.binary_path_python, "-m", "pip", "install", "--upgrade", "pip", "--user"])
-        subprocess.call([bpy.app.binary_path_python, "-m", "pip", "install", "boto3", "--user"])
+        if run_pip("--upgrade", "pip") and run_pip("boto3"):
+            # at this point library can be loaded by direct path usage, for example:
+            # hardcoded_path = "C:\\Users\\<user_name>\\AppData\\Roaming\\Python\\Python37\\site-packages"
+            # importlib.sys.path.append(hardcoded_path)
+            # now the "import boto3" or "globals()['boto3'] = importlib.import_module('boto3')" can be used to load it
+            # the path will also be added by Blender once it reloaded, after that no path modification would be needed.
 
-        # TODO: check if it available from other modules after that
-        globals()['boto3'] = importlib.import_module('boto3')
-        log.info("Library boto3 should be available right now. At least in install_libs module")
+            # now there is a question - how to adjust the sys.path at the firstmost install correctly for all OSs?
+            log.info("Library boto3 should be available right now. At least in install_libs module scope")
+        else:
+            log.warn("Something went wrong, unable to install boto3 library.")
