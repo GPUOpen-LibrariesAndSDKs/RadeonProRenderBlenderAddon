695126797ac594a5b8837433d149a8cb6f85fb1d
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2020-01-28 19:13:58 +0200

    Simplifying build_osx_installer.sh

--- BlenderPkg/build_osx_installer.sh
+++ BlenderPkg/build_osx_installer.sh
@@ -1,15 +1,10 @@
 rm -rf __pycache__
 
-rm ThirdParty
-ln -s ../ThirdParty ThirdParty
+pushd ..
 
-pushd ../
-
-python3.7 build.py
+./build_osx.sh
 
 popd
 
 # Parameters: --sign will prompt user for the signing id
 python3.7 build_osx_installer.py --nomatlib $1
-
-rm ThirdParty
