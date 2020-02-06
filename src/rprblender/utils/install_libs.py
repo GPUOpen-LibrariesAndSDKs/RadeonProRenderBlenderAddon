3e035b1c4966e37daa7dc371ed5c56caade36d3a
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-02-06 12:39:39 +0300

    Better explanation of Blender differences for Mac, Ubuntu and Win

--- src/rprblender/utils/install_libs.py
+++ src/rprblender/utils/install_libs.py
@@ -43,19 +43,25 @@ def ensure_boto3() -> None:
         import boto3
     except (ImportError, ModuleNotFoundError):
         log.info("Installing boto3 library...")
-        if platform.system() in ('Linux', 'Darwin'):
+        running_os = platform.system()
+        if running_os == 'Linux':  # Blender for linux has ensurepip module but no pip
             if run_python_call('ensurepip', '--upgrade') and run_pip("--upgrade", "pip") and run_pip('boto3'):
                 log.info("Library boto3 installed and ready to use.")
                 return
-        else:
+        elif running_os == 'Windows':  # Blender for Windows has pip and no ensurepip module
             if run_pip("--upgrade", "pip") and run_pip("boto3"):
-                # at this point library can be loaded by direct path usage, for example:
+                # Note: at this point library can be loaded by direct path usage, for example:
                 # hardcoded_path = "C:\\Users\\<user_name>\\AppData\\Roaming\\Python\\Python37\\site-packages"
                 # importlib.sys.path.append(hardcoded_path)
-                # now the "import boto3" or "globals()['boto3'] = importlib.import_module('boto3')" can be used to load it
+                # globals()['boto3'] = importlib.import_module('boto3')
 
                 # after Blender restart no path modification would be needed. Report and let it be.
                 log.info("Library boto3 should be available after Blender restart.")
+                return
+        else:
+            if run_pip("--upgrade", "pip") and run_pip("boto3"):
+                # Mac is fine.
+                log.info("Library boto3 installed and ready to use.")
                 return
 
         log.warn("Something went wrong, unable to install boto3 library.")
