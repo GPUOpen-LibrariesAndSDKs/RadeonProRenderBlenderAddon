ef0a50e6fbcda9008351cff93b9694cef21e899f
radeonprorender <radeonprorender.buildmaster@gmail.com>
2020-02-06 16:17:54 +0100

    buildmaster: version update to 2.2.13

--- src/rprblender/__init__.py
+++ src/rprblender/__init__.py
@@ -6,7 +6,7 @@ import bpy
 bl_info = {
     "name": "Radeon ProRender",
     "author": "AMD",
-    "version": (2, 2, 12),
+    "version": (2, 2, 13),
     "blender": (2, 80, 0),
     "location": "Info header, render engine menu",
     "description": "Radeon ProRender rendering plugin for Blender 2.8x",
