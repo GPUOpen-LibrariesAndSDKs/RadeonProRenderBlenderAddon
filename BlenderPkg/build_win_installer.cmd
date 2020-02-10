46f888433012cf0f651fd637afc2012d56f205b9
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2020-02-10 20:58:48 +0200

    RPRBLND-1200: Get rid of OpenCL lib in installer. (#496)
    PURPOSE
    OpenCL check is not needed anymore in installer for Windows, that's why it can be removed from it.
    
    EFFECT OF CHANGE
    Removed Check Devices tab from Installer in Windows ..

--- BlenderPkg/build_win_installer.cmd
+++ BlenderPkg/build_win_installer.cmd
@@ -19,7 +19,7 @@ echo Building Radeon ProRender AddOn for Blender %BLENDER_PLUGIN_VERSION%
 :cleanup
 del /F RadeonProRenderBlender_*.msi
 del /F addon.zip
-rmdir /S /Q __pycache__ out output system ThirdParty Installer\PluginInstaller\bin Installer\PluginInstaller\Generated Installer\PluginInstaller\obj
+rmdir /S /Q __pycache__ out output system Installer\PluginInstaller\bin Installer\PluginInstaller\Generated Installer\PluginInstaller\obj
 
 :: parse options
 if "%1"=="clean" goto :eof
@@ -36,16 +36,12 @@ if %ERRORLEVEL% equ 9009 (
 )
 
 :build_plugin
+
 pushd ..\
-
 call build.cmd
-
 popd
 
 :build_installer
-
-:: update ThirdParty
-xcopy /S /Y /I ..\ThirdParty\OpenCL\* ThirdParty\OpenCL
 
 :: copy files
 call py -3.7 build_win_installer.py
