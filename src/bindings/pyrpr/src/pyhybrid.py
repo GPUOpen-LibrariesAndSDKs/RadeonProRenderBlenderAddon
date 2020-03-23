51dd801b37c104ae516bd33e36a095f3f98dd786
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2020-03-24 00:18:52 +0200

    RPRBLND-1228: Hybrid mode unsupported features should not crash hybrid render. (#511)
    PURPOSE
    Some new features which become implemented in hybrid we can't automatically enable in plugin because they are explicitly unsupported there. That's why it'll be good to not use explicit unsupport, but still call core functions and ignore unsupported errors.
    
    EFFECT OF CHANGE
    Improved working with hybrid render, enabled some latest features.

--- src/bindings/pyrpr/src/pyhybrid.py
+++ src/bindings/pyrpr/src/pyhybrid.py
@@ -13,77 +13,58 @@
 # limitations under the License.
 #********************************************************************
 import numpy as np
+import functools
 
 import pyrpr
+from rprblender.config import hybrid_unsupported_log_warn
 
 from rprblender.utils import logging
 log = logging.Log(tag='hybrid')
 
 
-def log_unsupported(*msg):
-    log.debug("Unsupported", *msg)
+def ignore_unsupported(function):
+    """Function decorator which ignores UNSUPPORTED and INVALID_PARAMETER core errors"""
+
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+
+        except pyrpr.CoreError as e:
+            if e.status not in (pyrpr.ERROR_UNSUPPORTED, pyrpr.ERROR_INVALID_PARAMETER):
+                raise
+
+            if hybrid_unsupported_log_warn:
+                log.warn("Unsupported", function, *args)
+
+    return wrapper
+
+
+def class_ignore_unsupported(cls):
+    """Class decorator which decorates class functions by ignore_unsupported decorator"""
+
+    for attr_name in dir(cls):
+        # we are going to decorate all functions in classes with name not started with "__"
+        if attr_name.startswith("__"):
+            continue
+
+        attr = getattr(cls, attr_name)
+        if callable(attr):
+            setattr(cls, attr_name, ignore_unsupported(attr))
+
+    return cls
 
 
 # will be used in other modules to check if hybrid is enabled
 enabled = True
 
-UNSUPPORTED_CONTEXT_PARAMETERS = {
-    pyrpr.CONTEXT_MAX_DEPTH_DIFFUSE,
-    pyrpr.CONTEXT_MAX_DEPTH_GLOSSY,
-    pyrpr.CONTEXT_MAX_DEPTH_REFRACTION,
-    pyrpr.CONTEXT_MAX_DEPTH_SHADOW,
-    pyrpr.CONTEXT_MAX_DEPTH_GLOSSY_REFRACTION,
-    pyrpr.CONTEXT_RADIANCE_CLAMP,
-    pyrpr.CONTEXT_RAY_CAST_EPISLON,
 
-    pyrpr.CONTEXT_ADAPTIVE_SAMPLING_TILE_SIZE,
-    pyrpr.CONTEXT_ADAPTIVE_SAMPLING_MIN_SPP,
-    pyrpr.CONTEXT_ADAPTIVE_SAMPLING_THRESHOLD,
-    pyrpr.CONTEXT_FRAMECOUNT,
-
-    pyrpr.CONTEXT_PREVIEW,
-    pyrpr.CONTEXT_RENDER_MODE,
-
-    pyrpr.CONTEXT_IMAGE_FILTER_TYPE,
-    pyrpr.CONTEXT_IMAGE_FILTER_BOX_RADIUS,
-    pyrpr.CONTEXT_IMAGE_FILTER_GAUSSIAN_RADIUS,
-    pyrpr.CONTEXT_IMAGE_FILTER_TRIANGLE_RADIUS,
-    pyrpr.CONTEXT_IMAGE_FILTER_MITCHELL_RADIUS,
-    pyrpr.CONTEXT_IMAGE_FILTER_LANCZOS_RADIUS,
-    pyrpr.CONTEXT_IMAGE_FILTER_BLACKMANHARRIS_RADIUS,
-}
-
-SUPPORTED_MATERIAL_NODES = {
-    pyrpr.MATERIAL_NODE_NORMAL_MAP,
-    pyrpr.MATERIAL_NODE_IMAGE_TEXTURE,
-    pyrpr.MATERIAL_NODE_BUMP_MAP,
-    pyrpr.MATERIAL_NODE_ARITHMETIC,
-    pyrpr.MATERIAL_NODE_CONSTANT_TEXTURE,
-    pyrpr.MATERIAL_NODE_UBERV2,
-    pyrpr.MATERIAL_NODE_EMISSIVE
-}
-
-SUPPORTED_AOVS = {
-    pyrpr.AOV_COLOR,
-    pyrpr.AOV_DEPTH,
-    pyrpr.AOV_UV,
-    pyrpr.AOV_OBJECT_ID,
-    pyrpr.AOV_MATERIAL_IDX,
-    pyrpr.AOV_WORLD_COORDINATE,
-    pyrpr.AOV_SHADING_NORMAL,
-    pyrpr.AOV_DIFFUSE_ALBEDO,
-    pyrpr.AOV_BACKGROUND,
-}
-
-
+# @class_ignore_unsupported
 class Context(pyrpr.Context):
+    @ignore_unsupported
     def set_parameter(self, key, param):
         if key == pyrpr.CONTEXT_ITERATIONS:
             self.parameters[key] = param
-            return
-
-        if key in UNSUPPORTED_CONTEXT_PARAMETERS:
-            log_unsupported(f"Context.set_parameter({key})")
             return
 
         super().set_parameter(key, param)
@@ -93,28 +74,17 @@ class Context(pyrpr.Context):
         for _ in range(iterations):
             super().render()
 
+    @ignore_unsupported
     def attach_aov(self, aov, frame_buffer):
-        if aov in self.aovs:
-            self.detach_aov(aov)
+        super().attach_aov(aov, frame_buffer)
 
-        self.aovs[aov] = frame_buffer
-        frame_buffer.aov = aov
-        if aov in SUPPORTED_AOVS:
-            pyrpr.ContextSetAOV(self, aov, frame_buffer)
-        else:
-            log_unsupported(f"Context.attach_aov({aov})")
-
+    @ignore_unsupported
     def detach_aov(self, aov):
-        self.aovs[aov].aov = None
-        if aov in SUPPORTED_AOVS:
-            pyrpr.ContextSetAOV(self, aov, None)
-
-        del self.aovs[aov]
+        super().detach_aov(aov)
 
 
 class Light(pyrpr.Light):
-    def set_group_id(self, group_id):
-        log_unsupported("Light.set_group_id()")
+    pass
 
 
 class IESLight(pyrpr.PointLight, Light):
@@ -123,18 +93,19 @@ class IESLight(pyrpr.PointLight, Light):
         pass
 
 
+@class_ignore_unsupported
 class PointLight(pyrpr.PointLight, Light):
     pass
 
 
+@class_ignore_unsupported
 class SpotLight(pyrpr.SpotLight, Light):
     pass
 
 
+@class_ignore_unsupported
 class DirectionalLight(pyrpr.DirectionalLight, Light):
-    def set_shadow_softness_angle(self, angle):
-        log_unsupported("DirectionalLight.set_shadow_softness_angle()")
-        pass
+    pass
 
 
 class AreaLight(pyrpr.AreaLight, Light):
@@ -159,6 +130,7 @@ class AreaLight(pyrpr.AreaLight, Light):
             self.color_node.set_input(pyrpr.MATERIAL_INPUT_COLOR, 1.0)
 
 
+@class_ignore_unsupported
 class EnvironmentLight(pyrpr.EnvironmentLight, Light):
     def set_color(self, r, g, b):
         img = pyrpr.ImageData(self.context,
@@ -166,28 +138,12 @@ class EnvironmentLight(pyrpr.EnvironmentLight, Light):
         self.set_image(img)
 
 
+@class_ignore_unsupported
 class Camera(pyrpr.Camera):
-    def set_lens_shift(self, shiftx, shifty):
-        log_unsupported("Camera.set_lens_shift()")
-        pass
-
-    def set_aperture_blades(self, num_blades):
-        log_unsupported("Camera.set_aperture_blades()")
-        pass
-
-    def set_angular_motion(self, x, y, z, w):
-        log_unsupported("Camera.set_angular_motion()")
-        pass
-
-    def set_linear_motion(self, x, y, z):
-        log_unsupported("Camera.set_linear_motion()")
-        pass
-
-    def set_exposure(self, exposure):
-        log_unsupported("Camera.set_exposure()")
-        pass
+    pass
 
 
+@class_ignore_unsupported
 class MaterialNode(pyrpr.MaterialNode):
     def set_input(self, name, value):
         if isinstance(value, EmptyMaterialNode):
@@ -196,38 +152,7 @@ class MaterialNode(pyrpr.MaterialNode):
 
             value = 0.0
 
-        if self.type == pyrpr.MATERIAL_NODE_UBERV2 \
-            and name in (
-                pyrpr.MATERIAL_INPUT_UBER_BACKSCATTER_WEIGHT,
-                pyrpr.MATERIAL_INPUT_UBER_BACKSCATTER_COLOR,
-                pyrpr.MATERIAL_INPUT_UBER_REFLECTION_NORMAL,
-                pyrpr.MATERIAL_INPUT_UBER_REFRACTION_CAUSTICS,
-                pyrpr.MATERIAL_INPUT_UBER_REFRACTION_NORMAL,
-                pyrpr.MATERIAL_INPUT_UBER_SHEEN,
-                pyrpr.MATERIAL_INPUT_UBER_SHEEN_TINT,
-                pyrpr.MATERIAL_INPUT_UBER_SHEEN_WEIGHT,
-                pyrpr.MATERIAL_INPUT_UBER_COATING_TRANSMISSION_COLOR,
-                pyrpr.MATERIAL_INPUT_UBER_COATING_THICKNESS,
-                pyrpr.MATERIAL_INPUT_UBER_REFRACTION_ABSORPTION_DISTANCE,
-                pyrpr.MATERIAL_INPUT_UBER_REFRACTION_ABSORPTION_COLOR,
-                pyrpr.MATERIAL_INPUT_UBER_EMISSION_MODE,
-        ):
-            log_unsupported(f"MaterialNode.set_input({name})", self.type)
-            return
-
-        if self.type == pyrpr.MATERIAL_NODE_IMAGE_TEXTURE and name == pyrpr.MATERIAL_INPUT_UV:
-            log_unsupported(f"MaterialNode.set_input({name})", self.type)
-            return
-
-        try:
-            super().set_input(name, value)
-
-        except pyrpr.CoreError as e:
-            if e.status in (pyrpr.ERROR_UNSUPPORTED, pyrpr.ERROR_INVALID_PARAMETER):
-                log.warn(e, self.type)
-                return
-
-            raise
+        super().set_input(name, value)
 
 
 class EmptyMaterialNode(MaterialNode):
@@ -245,41 +170,17 @@ class EmptyMaterialNode(MaterialNode):
 
 
 class Shape(pyrpr.Shape):
-    def set_light_group_id(self, group_id):
-        log_unsupported("Shape.set_light_group_id()")
-        pass
+    def set_volume_material(self, material):
+        if isinstance(material, EmptyMaterialNode):
+            material = None
 
-    def set_visibility_primary_only(self, visible):
-        log_unsupported("Shape.set_visibility_primary_only()")
-        pass
+        super().set_volume_material(material)
 
-    def set_visibility_ex(self, visibility_type, visible):
-        log_unsupported("Shape.set_visibility_ex()")
-        pass
+    def set_displacement_material(self, material):
+        if isinstance(material, EmptyMaterialNode):
+            material = None
 
-    def set_visibility(self, visible):
-        log_unsupported("Shape.set_visibility()")
-        pass
-
-    def set_shadow_catcher(self, shadow_catcher):
-        log_unsupported("Shape.set_shadow_catcher()")
-        pass
-
-    def set_reflection_catcher(self, reflection_catcher):
-        log_unsupported("Shape.set_reflection_catcher()")
-        pass
-
-    def set_volume_material(self, node):
-        log_unsupported("Shape.set_volume_material()")
-        pass
-
-    def set_displacement_material(self, node):
-        log_unsupported("Shape.set_displacement_material()")
-        pass
-
-    def set_hetero_volume(self, hetero_volume):
-        log_unsupported("Shape.set_hetero_volume()")
-        pass
+        super().set_displacement_material(material)
 
     def set_material(self, material):
         if isinstance(material, EmptyMaterialNode):
@@ -287,38 +188,18 @@ class Shape(pyrpr.Shape):
 
         super().set_material(material)
 
-    def set_material_faces(self, material, face_indices: np.array):
-        log_unsupported("Shape.set_material_faces()")
-        self.set_material(material)
 
-    def set_vertex_value(self, index: int, indices, values):
-        log_unsupported("Shape.set_vertex_value()")
-        pass
-
-    def set_linear_motion(self, x, y, z):
-        log_unsupported("Shape.set_linear_motion()")
-        pass
-
-    def set_angular_motion(self, x, y, z, w):
-        log_unsupported("Shape.set_angular_motion()")
-        pass
-
-    def set_scale_motion(self, x, y, z):
-        log_unsupported("Shape.set_scale_motion()")
-        pass
-
-    def set_id(self, id):
-        log_unsupported("Shape.set_object_id()")
-
-
+@class_ignore_unsupported
 class Mesh(pyrpr.Mesh, Shape):
     pass
 
 
+@class_ignore_unsupported
 class Instance(pyrpr.Instance, Shape):
     pass
 
 
+@class_ignore_unsupported
 class Scene(pyrpr.Scene):
     def attach(self, obj):
         if isinstance(obj, (pyrpr.Curve, pyrpr.HeteroVolume)):
@@ -340,17 +221,6 @@ class Scene(pyrpr.Scene):
 
         super().clear()
 
-    def add_environment_override(self, core_id, light):
-        log_unsupported("Scene.add_environment_override()")
-        pass
-
-    def remove_environment_override(self, core_id):
-        log_unsupported("Scene.remove_environment_override()")
-        pass
-
-    def set_background_image(self, image):
-        log_unsupported("Scene.set_background_image()")
-        pass
 
 class PostEffect:
     def __init__(self, context, post_effect_type):
