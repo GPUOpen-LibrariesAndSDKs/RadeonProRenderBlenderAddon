4a418de8321cddaa1bda2ec4fcce5cd651eb67d8
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-08-13 20:55:10 +0300

    [RPRBLND-1419][RPRBLND-1422] Use RPR 2.0 Sphere and Disk lights for Point and Spot (#59)
    PURPOSE
    RPR 2.0 added physically correct light sources alternatives to Point Light and Spot Light - the Sphere Light and Disk Light.
    Use them when non-zero light size(radius) is used.
    
    EFFECT OF CHANGE
    
    added Shadow Soft Size to Point light and Spot light UI;
    RPR 2.0 uses more physically correct light render for Point and Spot light when Shadow Soft Size is set more than 0.
    TECHNICAL STEPS
    
    added Shadow Soft Size to Point light and Spot light UI, used existing Blender light property;
    added new light methods to wrapper builder conversion list;
    added RPR 2.0 Sphere and Disk light classes, used in RPR 2.0 mode;
    for compatibility added disk and sphere light placeholders in RPR 1.0 - used spot and point light for that;
    for compatibility added blank set_radius method to RPR 1.0 light base class;
    adjusted Sphere and Disk lights intensity using current light radius to keep the same result intesity;
    adjusted Sphere and Disk lights intensity on radius change.
    NOTES
    
    if light radius is big enough to intersect with illuminated object the dark areas will be present in areas of sphere intersection with said objects;
    Disk light result size changes with Disk light radius change;. softness doesn't change from angle., but raises from radius increase The different method of size calculation, that will account for angle(and visa-verse), is requred.

--- src/bindings/pyrpr/src/pyrpr2.py
+++ src/bindings/pyrpr/src/pyrpr2.py
@@ -1,7 +1,16 @@
+import math
 import numpy as np
 
 import pyrpr
-
+from pyrprwrap import (
+    ContextCreateSphereLight,
+    SphereLightSetRadiantPower3f,
+    SphereLightSetRadius,
+    ContextCreateDiskLight,
+    DiskLightSetRadiantPower3f,
+    DiskLightSetRadius,
+    DiskLightSetAngle,
+)
 
 # will be used in other modules to check if RPR 2 is enabled
 enabled = False
@@ -9,3 +18,49 @@ enabled = False
 
 class Context(pyrpr.Context):
     pass
+
+
+class SphereLight(pyrpr.Light):
+    def __init__(self, context):
+        super().__init__(context)
+        ContextCreateSphereLight(self.context, self)
+
+        # keep target intensity and radius to adjust actual intensity when they are changed
+        self._radius_squared = 1
+
+    def set_radiant_power(self, r, g, b):
+        # Adjust intensity by current radius
+        SphereLightSetRadiantPower3f(self,
+                                     r / self._radius_squared,
+                                     g / self._radius_squared,
+                                     b / self._radius_squared)
+
+    def set_radius(self, radius):
+        radius = max(radius, 0.01)
+        self._radius_squared = radius * radius
+        SphereLightSetRadius(self, radius)
+
+
+class DiskLight(pyrpr.Light):
+    def __init__(self, context):
+        super().__init__(context)
+        ContextCreateDiskLight(self.context, self)
+
+        # keep target intensity and radius to adjust actual intensity when they are changed
+        self._radius_squared = 1
+
+    def set_radiant_power(self, r, g, b):
+        # Adjust intensity by current radius
+        DiskLightSetRadiantPower3f(self,
+                                   r / self._radius_squared,
+                                   g / self._radius_squared,
+                                   b / self._radius_squared)
+
+    def set_cone_shape(self, iangle, oangle):
+        # Use external angle oangle
+        DiskLightSetAngle(self, oangle)
+
+    def set_radius(self, radius):
+        radius = max(radius, 0.01)
+        self._radius_squared = radius * radius
+        DiskLightSetRadius(self, radius)
