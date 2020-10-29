1a269e836e1d7a12fcdfb7bab163625651b5be1c
bsavery <brian.savery@gmail.com>
2020-10-29 13:12:34 -0700

    Rename BLENDER_28x_EXE -> BLENDER_EXE (#94)

--- run_blender_with_rpr.cmd
+++ run_blender_with_rpr.cmd
@@ -17,12 +17,12 @@ REM  *******************************************************************
 
 @echo on
 
-if ""=="%BLENDER_28x_EXE%" goto error
+if ""=="%BLENDER_EXE%" goto error
 
-py cmd_tools/run_blender.py "%BLENDER_28x_EXE%" cmd_tools/test_rpr.py
+py cmd_tools/run_blender.py "%BLENDER_EXE%" cmd_tools/test_rpr.py
 pause
 REM it's much easier to get issue traceback on crash if pause is present; remove if not needed
 exit
 
 :error
-echo "Please set BLENDER_28x_EXE environment variable"
+echo "Please set BLENDER_EXE environment variable"
