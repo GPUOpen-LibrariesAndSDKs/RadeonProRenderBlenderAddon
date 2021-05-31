91ee33742d7f23ef9801728be21821335b7e479b
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2021-05-31 18:42:46 +0300

    RPRBLND-1734: Issue in starting plugin when Boto3 failed to install. (#278)
    Implemented behavior: if Boto3 failed to install next try to install will be after 5 days.

--- src/rprblender/utils/install_libs.py
+++ src/rprblender/utils/install_libs.py
@@ -15,12 +15,18 @@
 import sys
 import site
 import subprocess
+from datetime import datetime, timedelta
 
 import bpy
-from . import IS_MAC, IS_LINUX
+from . import IS_MAC, IS_LINUX, package_root_dir
+from rprblender import config
 
 from rprblender.utils.logging import Log
 log = Log(tag="install_libs")
+
+
+PIP_CHECK_FILENAME = "pip_check.txt"
+NEXT_TIME_CHECK_DELTA = 5   # 5 days
 
 
 # adding user site-packages path to sys.path
@@ -47,10 +53,20 @@ def ensure_boto3():
     Use this snippet to install boto3 library with all the dependencies if absent at the addon launch time
     Note: still it will be available at the next Blender launch only
     """
+    pip_check_file = package_root_dir() / PIP_CHECK_FILENAME
+
     try:
         import boto3
 
     except ImportError:
+        # checking if we need to install boto3
+        if pip_check_file.is_file():
+            str_time = pip_check_file.read_text()
+            next_time_check = datetime.fromisoformat(str_time)
+            if datetime.now() < next_time_check:
+                config.disable_athena_report = True
+                return
+
         log.info("Installing boto3 library...")
         try:
             if IS_MAC or IS_LINUX:
@@ -64,3 +80,12 @@ def ensure_boto3():
 
         except subprocess.SubprocessError as e:
             log.warn("Something went wrong, unable to install boto3 library.", e)
+
+            # after failing installation of boto3 set next date to try install boto3
+            next_time_check = datetime.now() + timedelta(NEXT_TIME_CHECK_DELTA)
+            pip_check_file.write_text(next_time_check.isoformat())
+            config.disable_athena_report = True
+            return
+
+    if pip_check_file.is_file():
+        pip_check_file.unlink()
