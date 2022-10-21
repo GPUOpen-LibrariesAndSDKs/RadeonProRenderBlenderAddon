a17dc25acfe04b16b78e6f5fc49e97ea7007b512
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2022-10-21 18:44:34 +0300

    RPRBLND-2211: Add new HybridPro settings. (#552)
    PURPOSE
    
    HybridPro render quality provides recommended settings for viewport and final render.
    
    EFFECT OF CHANGE
    
    Improved 'Interactive' render quality by adding recommended settings.

--- src/rprblender/engine/render_engine_hybridpro.py
+++ src/rprblender/engine/render_engine_hybridpro.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #********************************************************************
-import pyrpr
-
 from . import render_engine
 from . import context_hybridpro
 
