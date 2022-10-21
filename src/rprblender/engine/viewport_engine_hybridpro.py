daae13fc412ff137e113dd80aee0d779d8358598
Bogdan Nagirniak <33626169+bnagirniak@users.noreply.github.com>
2022-10-21 18:44:34 +0300

    RPRBLND-2211: Add new HybridPro settings. (#552)
    PURPOSE
    
    HybridPro render quality provides recommended settings for viewport and final render.
    
    EFFECT OF CHANGE
    
    Improved 'Interactive' render quality by adding recommended settings.

--- src/rprblender/engine/viewport_engine_hybridpro.py
+++ src/rprblender/engine/viewport_engine_hybridpro.py
@@ -15,6 +15,8 @@
 from . import viewport_engine
 from . import context_hybridpro
 
+import pyrpr
+
 from rprblender.operators.world import FOG_KEY
 
 from rprblender.utils import logging
@@ -28,6 +30,7 @@ class ViewportEngine(viewport_engine.ViewportEngine):
         super().__init__(rpr_engine)
 
         self.render_image = None
+        self.is_denoised = False
 
     def _resolve(self):
         self.render_image = self.rpr_context.get_image()
@@ -41,3 +44,31 @@ class ViewportEngine(viewport_engine.ViewportEngine):
                 continue
 
             yield obj
+
+    def sync(self, context, depsgraph):
+        super().sync(context, depsgraph)
+
+        self.rpr_context.set_parameter(pyrpr.CONTEXT_MATERIAL_CACHE, True)
+        self.rpr_context.set_parameter(pyrpr.CONTEXT_RESTIR_GI, True)
+        self.rpr_context.set_parameter(pyrpr.CONTEXT_RESTIR_GI_BIAS_CORRECTION, 3)
+        self.rpr_context.set_parameter(pyrpr.CONTEXT_RESERVOIR_SAMPLING, 2)
+        self.rpr_context.set_parameter(pyrpr.CONTEXT_RESTIR_SPATIAL_RESAMPLE_ITERATIONS, 3)
+        self.rpr_context.set_parameter(pyrpr.CONTEXT_RESTIR_MAX_RESERVOIRS_PER_CELL, 128)
+
+        log("Finish sync")
+
+    def setup_image_filter(self, settings):
+        self.is_denoised = settings['enable']
+        self.rpr_context.set_parameter(pyrpr.CONTEXT_PT_DENOISER,
+                                       pyrpr.DENOISER_SVGF if self.is_denoised else pyrpr.DENOISER_NONE)
+
+    def notify_status(self, info, status):
+        # Adding " | Denoised" to status message
+        if self.is_denoised and status in ("Render", "Rendering Done") and info != "Starting...":
+            upscaled_index = info.rfind(" | Upscaled")
+            if upscaled_index >= 0:
+                info = info[:upscaled_index] + " | Denoised" + info[upscaled_index:]
+            else:
+                info += " | Denoised"
+
+        super().notify_status(info, status)
