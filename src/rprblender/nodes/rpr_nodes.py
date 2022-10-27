9486cdc7a769ec81547df1e534cb1c424264f6c9
Vasyl Pidhirskyi <42581166+VascoPi@users.noreply.github.com>
2022-10-27 18:15:39 +0300

    RPRBLND-2202: Add support for bevel shader node (including RPRBLND-2201) (#551)
    PURPOSE
    Add support for bevel shader node.
    Verify other nodes.
    
    EFFECT OF CHANGE
    Added Bevel Node for Final renderer.
    Improved Bump node for Final renderer.
    Added 'LOG', 'SHUFFLE_YZWX', 'SHUFFLE_ZWXY', 'SHUFFLE_WXYZ' operation for RPR Math for Interactive renderer.
    Added procedural_type 'Triplanar' for RPR Procedural UV for Interactive renderer.
    Added support for Checker Texture for Interactive renderer.
    Fixed Anisotropic BSDF for Interactive renderer.

--- src/rprblender/nodes/rpr_nodes.py
+++ src/rprblender/nodes/rpr_nodes.py
@@ -803,6 +803,7 @@ class RPRShaderProceduralUVNode(RPRShaderNode):
         we use this for both shapes and triplanar
     """
     bl_label = 'RPR Procedural UV'
+    bl_width_min = 260
 
     procedural_type: EnumProperty(
         name='Type',
@@ -919,6 +920,15 @@ class RPRShaderProceduralUVNode(RPRShaderNode):
 
         def export_hybrid(self):
             return None
+
+        def export_hybridpro(self):
+            procedural_type = self.node.procedural_type
+            if procedural_type != 'TRIPLANAR':
+                log.warn("Ignoring unsupported RPR procedural type",
+                         procedural_type, self.node, self.material)
+                return None
+
+            return self.export()
 
 
 class RPRShaderNodeBumpMap(RPRShaderNode):
@@ -1525,6 +1535,9 @@ class RPRValueNode_Math(RPRShaderNode):
                          op, self.node, self.material)
                 return None
 
+            return self.export()
+
+        def export_hybridpro(self):
             return self.export()
 
 
