aa21e5d35e9abd446291f1c7dc2e8247723a7ed8
takahiroharada <takahiroharada@gmail.com>
2024-05-23 17:09:45 -0700

    Fix macos arm build.

--- build_osx-arm64.sh
+++ build_osx-arm64.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-arch -arm64 python3.9 cmd_tools/create_sdk.py
+arch -arm64 python3.11 cmd_tools/create_sdk.py
 
 IGNORE_MISSING_OPENMP=1
 cxml="/usr/local/bin/castxml"
 if [ -f "$cxml" ]; then
-	arch -arm64 python3.9 src/bindings/pyrpr/src/pyrprapi.py $cxml
+	arch -arm64 python3.11 src/bindings/pyrpr/src/pyrprapi.py $cxml
 	if [ -f "./bindings-ok" ]; then
-  	arch -arm64 python3.9 build.py
-  	arch -arm64 python3.10 build.py
+  	arch -arm64 python3.11 build.py
 	else
 		echo Compiling bindings failed
 	fi
