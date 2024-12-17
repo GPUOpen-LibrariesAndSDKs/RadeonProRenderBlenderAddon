45b30c775aab61e39e7126b58c4227b9eb92ae94
takahiroharada <takahiroharada@gmail.com>
2024-12-17 15:25:18 -0800

    Low ray depth for fast.

--- src/rprblender/properties/presets/viewport/full2/fast.py
+++ src/rprblender/properties/presets/viewport/full2/fast.py
@@ -2,12 +2,12 @@ import bpy
 
 rpr = bpy.context.scene.rpr
 
-rpr.viewport_ray_depth.max_ray_depth = 6
-rpr.viewport_ray_depth.diffuse_depth = 5
-rpr.viewport_ray_depth.glossy_depth = 6
-rpr.viewport_ray_depth.shadow_depth = 5
-rpr.viewport_ray_depth.refraction_depth = 6
-rpr.viewport_ray_depth.glossy_refraction_depth = 5
+rpr.viewport_ray_depth.max_ray_depth = 2
+rpr.viewport_ray_depth.diffuse_depth = 2
+rpr.viewport_ray_depth.glossy_depth = 2
+rpr.viewport_ray_depth.shadow_depth = 2
+rpr.viewport_ray_depth.refraction_depth = 2
+rpr.viewport_ray_depth.glossy_refraction_depth = 2
 
 rpr.viewport_limits.min_samples = 16
 rpr.viewport_limits.max_samples = 32
